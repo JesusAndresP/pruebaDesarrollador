e924dfcace33632b6aa14cae436221cfa41da72e
JesusAndresP <jeanpiro23@outlook.com>
2021-09-14 18:00:12 -0500

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { PublicacionesComponent } from './components/publicaciones/publicaciones.component';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  { path: 'publicaciones', component: PublicacionesComponent },
+  { path: 'usuarios', component: UsuariosComponent },
+  { path: 'inicio', component: InicioComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
